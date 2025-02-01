07e73520369ae52587f66657d2676e41cfdb51cd
KazemotoKenzo <kenzo80804@gmail.com>
2025-02-01 02:08:35 -0300

    limitando descricao

--- src/containers/ProductItem/index.tsx
+++ src/containers/ProductItem/index.tsx
@@ -12,11 +12,18 @@ type Props = {
 }
 
 const ProductItem = ({ image, title, description }: Props) => {
+  const getDescription = (d: string) => {
+    if (d.length > 216) {
+      return d.slice(0, 216) + '...'
+    } else {
+      return d
+    }
+  }
   return (
     <>
       <ProductImage src={image} />
       <ProductTitle>{title}</ProductTitle>
-      <ProductDescription>{description}</ProductDescription>
+      <ProductDescription>{getDescription(description)}</ProductDescription>
       <AddProduct>Adicionar ao carrinho</AddProduct>
     </>
   )
